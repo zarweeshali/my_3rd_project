3368710ba46ae6ed85a7dfda7a6823700f5446cd
Zarweesh Ali <165051758+zarweeshali@users.noreply.github.com>
2025-04-05 20:13:25 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+st.title("Growth Mindset AI Project.")
+st.markdown("### Welcome to Your Growth Journey!")
+st.write(" Embrance Challenges, learn from Mistake , And Unlock Your Full Potential. This is AI-Powered AppHelps you blind a Growth Mindset with reflection challanges And Achivement!")
+
+st.title("What will you Conquer today?")
+st.write(" Every day is a opportunity to growth.Set your challenge and rise above it!")
+st.title("What's Your Challenges Today?")
+name = st.text_input("Describe a challenge you are facing?")
+st.button("Tell us about Your Chhallanges to get started!")
+st.title("Turn Setbacks into a Comebacks!")
+st.write("Faliures are just Stepping stones.Every setback is a set for a comeback!")
+st.title("Reflecting on experience helps you grow! Write your Throughts.")
+name = st.text_input("Write your reflects here:")
+st.button("Reflection on Past experience help to you grow! "
+"share your Difficulties!")
+st.title("Push Past Limits, Unlock New Level!")
+st.write("The best version of you is on the other side of your comfort zone.keep going!")
+st.title("Celebrate Your Achivement! Not Perfection")
+first_name = st.text_input("Share something You have Recently Accomplishment by the Grace of Allah!")
+st.button("Big or small, light or darker, every achievement counts!"
+" Just trust in Allah plan"
+" and keep moving forwading!")
+
+st.button("Growth begins at the end of your comfort zone.Every challanges is a chance to learn, adapt, and rise stronger!")
